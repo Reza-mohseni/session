6e93603f5cfd4cafdd39d7a842ea9d3a71d43acd
Reza Mohseni <mohseni.rezaofficial@gmail.com>
2024-01-11 22:19:34 +0330

    تمرین 2

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-$cars=array("x22",'x33','lx10','xl60');
+/*$cars=array("x22",'x33','lx10','xl60');
 $name=array('reza','ali','hamide','reyhane','alireza');
 var_dump($cars);
-var_dump($name);
+var_dump($name);*/
+
+
+$str = 'one|two|three|four';
+
+// positive limit
+print_r(explode('|', $str, 3));
+echo '<br>';
+// negative limit
+print_r(explode('|', $str, -5));
